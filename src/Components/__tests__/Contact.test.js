21fbbd0b8832487c21bab1e365a853eb7a4e131b
itisameerkhan <itisameerkhan@gmail.com>
2023-10-10 18:11:06 +0530

    10/10/2023 18:11

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -2,18 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Contact from '../Contact/Contact.jsx';
 import "@testing-library/jest-dom";
 
-test("should load contact use component", () => {
-    render(<Contact />)
-    const heading = screen.getByRole("heading");
+
+describe("contact us page test cases", () => {
+
+    it("should load contact use component", () => {
+        render(<Contact />)
+        const heading = screen.getByRole("heading");
+        
+        expect(heading).toBeInTheDocument();
+    })
     
-    expect(heading).toBeInTheDocument();
-})
-
-test("should load 2 input boxes on the contact components", () => {
-    render(<Contact />)
-
-    const inputBoxes = screen.getAllByRole("textbox");
-    console.log(inputBoxes);
-
-    expect(inputBoxes.length).toBe(2);
+    it("should load 2 input boxes on the contact components", () => {
+        render(<Contact />)
+    
+        const inputBoxes = screen.getAllByRole("textbox");
+    
+        expect(inputBoxes.length).toBe(2);
+    })
 })
